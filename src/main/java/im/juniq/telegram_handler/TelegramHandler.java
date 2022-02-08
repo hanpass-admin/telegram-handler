5ec919274d177173fddc5f7ef64a769a8b39408c
juniq <juniqlim@gmail.com>
2022-02-08 12:21:08 +0900

    fix: 핸들링하는 전문 타입을 byte[]로 변경

--- src/main/java/im/juniq/telegram_handler/TelegramHandler.java
+++ src/main/java/im/juniq/telegram_handler/TelegramHandler.java
@@ -1,39 +1,59 @@
 package im.juniq.telegram_handler;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class TelegramHandler {
-    private String telegram;
+    private final byte[] telegram;
+    private final Charset charset;
 
     public TelegramHandler(String telegram) {
+        charset = StandardCharsets.UTF_8;
+        this.telegram = telegram.getBytes(charset);
+    }
+    public TelegramHandler(String telegram, Charset charset) {
+        this.charset = charset;
+        this.telegram = telegram.getBytes(charset);
+    }
+
+    public TelegramHandler(byte[] telegram, Charset charset) {
+        this.charset = charset;
         this.telegram = telegram;
     }
 
     public String field(int beginIndex, int endIndex) {
         if (beginIndex < 1) {
-            throw new RuntimeException("beginIndex는 0보다 커야합니다. telegram length: " + telegram.length() + ", bedinIndex: " + beginIndex + ", endIndex: " + endIndex);
+            throw new RuntimeException("beginIndex는 0보다 커야합니다. telegram length: " + telegram.length + ", bedinIndex: " + beginIndex + ", endIndex: " + endIndex);
         }
 
-        if (endIndex > telegram.length()) {
-            throw new RuntimeException("endIndex가 전문길이보다 큽니다. telegram length: " + telegram.length() + ", bedinIndex: " + beginIndex + ", endIndex: " + endIndex);
+        if (endIndex > telegram.length) {
+            throw new RuntimeException("endIndex가 전문길이보다 큽니다. telegram length: " + telegram.length + ", bedinIndex: " + beginIndex + ", endIndex: " + endIndex);
         }
 
         try {
-            return telegram.substring(beginIndex - 1, endIndex);
+            return new String(extractByte(beginIndex, endIndex), charset);
         } catch (StringIndexOutOfBoundsException e) {
-            throw new RuntimeException("전문길이와 index가 맞지 않습니다. telegram length: " + telegram.length() + ", bedinIndex: " + beginIndex + ", endIndex: " + endIndex);
+            throw new RuntimeException("전문길이와 index가 맞지 않습니다. telegram length: " + telegram.length + ", bedinIndex: " + beginIndex + ", endIndex: " + endIndex);
         }
     }
 
+    private byte[] extractByte(int beginIndex, int endIndex) {
+        int length = endIndex - beginIndex + 1;
+        byte[] field = new byte[length];
+        System.arraycopy(telegram, beginIndex - 1, field, 0, length);
+        return field;
+    }
+
     public int isLengthOf(int length) {
-        return Integer.compare(length, telegram.length());
+        return Integer.compare(length, telegram.length);
     }
 
     public void changeField(int beginIndex, int endIndex, String newField) {
         if (endIndex - beginIndex != newField.length() - 1) {
             throw new RuntimeException("새로운 필드의 길이는 이전 필드의 길이와 같아야 합니다.");
         }
-        StringBuilder builder = new StringBuilder(telegram);
-        builder.delete(beginIndex - 1, endIndex);
-        builder.insert(beginIndex - 1, newField);
-        telegram = builder.toString();
+
+        byte[] field = newField.getBytes(charset);
+        System.arraycopy(field, 0, telegram, beginIndex - 1, endIndex - beginIndex + 1);
     }
 }
--- src/test/java/im/juniq/telegram_handler/TelegramHandlerTest.java
+++ src/test/java/im/juniq/telegram_handler/TelegramHandlerTest.java
@@ -4,10 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.nio.charset.Charset;
 import org.junit.jupiter.api.Test;
 
 class TelegramHandlerTest {
-    public static final String TEST_TELEGRAM = "12345testname  한글";
+    public static final String TEST_TELEGRAM = "12345testname";
+    public static final String TEST_TELEGRAM_WITH_KOREAN = "12345testname  한글";
 
     @Test
     void 전문핸들러_생성() {
@@ -18,35 +20,68 @@ class TelegramHandlerTest {
     void 전문길이_비교() {
         TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM);
 
-        assertThat(telegramHandler.isLengthOf(17)).isEqualTo(0);
-        assertThat(telegramHandler.isLengthOf(18)).isEqualTo(1);
-        assertThat(telegramHandler.isLengthOf(16)).isEqualTo(-1);
+        assertThat(telegramHandler.isLengthOf(13)).isEqualTo(0);
+        assertThat(telegramHandler.isLengthOf(14)).isEqualTo(1);
+        assertThat(telegramHandler.isLengthOf(12)).isEqualTo(-1);
+
+        TelegramHandler koreanTelegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
+
+        assertThat(koreanTelegramHandler.isLengthOf(21)).isEqualTo(0);
+    }
+
+    @Test
+    void 전문길이_비교_한글_UTF8() {
+        TelegramHandler koreanTelegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
+
+        assertThat(koreanTelegramHandler.isLengthOf(21)).isEqualTo(0);
+        assertThat(koreanTelegramHandler.isLengthOf(22)).isEqualTo(1);
+        assertThat(koreanTelegramHandler.isLengthOf(20)).isEqualTo(-1);
+    }
+
+    @Test
+    void 전문길이_비교_한글_EUCKR() {
+        TelegramHandler koreanTelegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN, Charset.forName("EUC-KR"));
+
+        assertThat(koreanTelegramHandler.isLengthOf(19)).isEqualTo(0);
+        assertThat(koreanTelegramHandler.isLengthOf(20)).isEqualTo(1);
+        assertThat(koreanTelegramHandler.isLengthOf(18)).isEqualTo(-1);
     }
 
     @Test
     void 특정필드_가져오기() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM);
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
         assertThat(telegramHandler.field(1, 5)).isEqualTo("12345");
         assertThat(telegramHandler.field(6, 13)).isEqualTo("testname");
         assertThat(telegramHandler.field(1, 1)).isEqualTo("1");
         assertThat(telegramHandler.field(6, 6)).isEqualTo("t");
-        assertThat(telegramHandler.field(16, 17)).isEqualTo("한글");
+        assertThat(telegramHandler.field(16, 21)).isEqualTo("한글");
+    }
+
+    @Test
+    void 특정필드_가져오기_EUCKR() {
+        TelegramHandler koreanTelegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN, Charset.forName("EUC-KR"));
+
+        assertThat(koreanTelegramHandler.field(1, 5)).isEqualTo("12345");
+        assertThat(koreanTelegramHandler.field(6, 13)).isEqualTo("testname");
+        assertThat(koreanTelegramHandler.field(1, 1)).isEqualTo("1");
+        assertThat(koreanTelegramHandler.field(6, 6)).isEqualTo("t");
+        assertThat(koreanTelegramHandler.field(16, 19)).isEqualTo("한글");
     }
 
     @Test
     void 특정필드_가져올때_전문길이_예외() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM);
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
-        assertThatThrownBy(() -> telegramHandler.field(6, 18)).isInstanceOf(RuntimeException.class)
-            .hasMessage("endIndex가 전문길이보다 큽니다. telegram length: 17, bedinIndex: 6, endIndex: 18");
-        assertThatThrownBy(() -> telegramHandler.field(0, 18)).isInstanceOf(RuntimeException.class)
-            .hasMessage("beginIndex는 0보다 커야합니다. telegram length: 17, bedinIndex: 0, endIndex: 18");
+        assertThatThrownBy(() -> telegramHandler.field(6, 22)).isInstanceOf(RuntimeException.class)
+            .hasMessage("endIndex가 전문길이보다 큽니다. telegram length: 21, bedinIndex: 6, endIndex: 22");
+        assertThatThrownBy(() -> telegramHandler.field(0, 22)).isInstanceOf(RuntimeException.class)
+            .hasMessage("beginIndex는 0보다 커야합니다. telegram length: 21, bedinIndex: 0, endIndex: 22");
     }
 
     @Test
     void 특정필드_값_바꾸기() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM);
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
         telegramHandler.changeField(1, 5, "54321");
 
@@ -56,7 +91,7 @@ class TelegramHandlerTest {
 
     @Test
     void 특정필드_값_바꾸기2() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM);
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
         telegramHandler.changeField(6, 9, "eeee");
 
@@ -66,7 +101,7 @@ class TelegramHandlerTest {
 
     @Test
     void 특정필드_값_바꾸기_필드길이_예외() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM);
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
         assertThatThrownBy(() -> telegramHandler.changeField(1, 5, "5432")).isInstanceOf(RuntimeException.class)
             .hasMessage("새로운 필드의 길이는 이전 필드의 길이와 같아야 합니다.");
