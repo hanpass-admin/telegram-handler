3600fa34868265ae381a1e3d3a7480a0bb8a5b77
juniq <juniqlim@gmail.com>
2022-02-15 11:49:27 +0900

    test: 테스트 코드 정리

--- src/test/java/im/juniq/telegram_handler/TelegramHandlerTest.java
+++ src/test/java/im/juniq/telegram_handler/TelegramHandlerTest.java
@@ -16,32 +16,13 @@ class TelegramHandlerTest {
     }
 
     @Test
-    void 전문길이_비교_UTF8() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
-
-        assertThat(telegramHandler.isLengthOf(50)).isEqualTo(0);
-        assertThat(telegramHandler.isLengthOf(51)).isEqualTo(1);
-        assertThat(telegramHandler.isLengthOf(49)).isEqualTo(-1);
-    }
-
-    @Test
-    void 전문길이_비교_EUCKR() {
-        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN, Charset.forName("EUC-KR"));
-
-        assertThat(telegramHandler.isLengthOf(48)).isEqualTo(0);
-        assertThat(telegramHandler.isLengthOf(49)).isEqualTo(1);
-        assertThat(telegramHandler.isLengthOf(47)).isEqualTo(-1);
-    }
-
-    @Test
     void 특정필드_가져오기() {
         TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
         assertThat(telegramHandler.field(1, 5)).isEqualTo("12345");
-        assertThat(telegramHandler.field(6, 8)).isEqualTo("testname");
-        assertThat(telegramHandler.field(1, 1)).isEqualTo("1");
-        assertThat(telegramHandler.field(6, 1)).isEqualTo("t");
+        assertThat(telegramHandler.field(6, 10)).isEqualTo("testname");
         assertThat(telegramHandler.field(16, 6)).isEqualTo("한글");
+        assertThat(telegramHandler.field(16, 7)).isEqualTo("한글");
         assertThat(telegramHandler.field(23, 3)).isEqualTo("321");
     }
 
@@ -50,10 +31,9 @@ class TelegramHandlerTest {
         TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN, Charset.forName("EUC-KR"));
 
         assertThat(telegramHandler.field(1, 5)).isEqualTo("12345");
-        assertThat(telegramHandler.field(6, 8)).isEqualTo("testname");
-        assertThat(telegramHandler.field(1, 1)).isEqualTo("1");
-        assertThat(telegramHandler.field(6, 1)).isEqualTo("t");
+        assertThat(telegramHandler.field(6, 10)).isEqualTo("testname");
         assertThat(telegramHandler.field(16, 4)).isEqualTo("한글");
+        assertThat(telegramHandler.field(16, 5)).isEqualTo("한글");
         assertThat(telegramHandler.field(21, 3)).isEqualTo("321");
     }
 
@@ -85,6 +65,7 @@ class TelegramHandlerTest {
 
         telegramHandler.changeField(1, 5, "eee");
 
+        assertThat(telegramHandler.originField(1, 5)).isEqualTo("eee  ");
         assertThat(telegramHandler.field(1, 5)).isEqualTo("eee");
     }
 
@@ -95,6 +76,7 @@ class TelegramHandlerTest {
         telegramHandler.changeFieldAndLeftFill(1, 5, "eee");
 
         assertThat(telegramHandler.originField(1, 5)).isEqualTo("  eee");
+        assertThat(telegramHandler.field(1, 5)).isEqualTo("eee");
     }
 
     @Test
@@ -111,6 +93,7 @@ class TelegramHandlerTest {
         telegramHandler.changeNumberField(31, 10, "77000");
 
         assertThat(telegramHandler.longField(31, 10)).isEqualTo(77000L);
+        assertThat(telegramHandler.originField(31, 10)).isEqualTo("0000077000");
     }
 
     @Test
@@ -119,5 +102,23 @@ class TelegramHandlerTest {
 
         assertThatThrownBy(() -> telegramHandler.changeField(1, 3, "5432")).isInstanceOf(RuntimeException.class)
             .hasMessage("새로운 필드의 길이는 이전 필드의 길이보다 작아야 합니다.");
+    }
+
+    @Test
+    void 전문길이_비교_UTF8() {
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
+
+        assertThat(telegramHandler.isLengthOf(50)).isEqualTo(0);
+        assertThat(telegramHandler.isLengthOf(51)).isEqualTo(1);
+        assertThat(telegramHandler.isLengthOf(49)).isEqualTo(-1);
+    }
+
+    @Test
+    void 전문길이_비교_EUCKR() {
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN, Charset.forName("EUC-KR"));
+
+        assertThat(telegramHandler.isLengthOf(48)).isEqualTo(0);
+        assertThat(telegramHandler.isLengthOf(49)).isEqualTo(1);
+        assertThat(telegramHandler.isLengthOf(47)).isEqualTo(-1);
     }
 }
