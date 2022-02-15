d55d1cb09e6973384e6f7486b1a32e185de7e6a0
juniq <juniqlim@gmail.com>
2022-02-15 11:45:55 +0900

    test: left pad 필드값 바꾸기 테스트 추가

--- src/test/java/im/juniq/telegram_handler/TelegramHandlerTest.java
+++ src/test/java/im/juniq/telegram_handler/TelegramHandlerTest.java
@@ -89,6 +89,15 @@ class TelegramHandlerTest {
     }
 
     @Test
+    void 모자란부분은_왼쪽에_스페이스로_채워서_특정필드_값_바꾸기() {
+        TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
+
+        telegramHandler.changeFieldAndLeftFill(1, 5, "eee");
+
+        assertThat(telegramHandler.originField(1, 5)).isEqualTo("  eee");
+    }
+
+    @Test
     void 특정필드_숫자로_가져오기() {
         TelegramHandler telegramHandler = new TelegramHandler(TEST_TELEGRAM_WITH_KOREAN);
 
